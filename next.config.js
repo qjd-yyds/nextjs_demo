091e3a59e752f336053c03a9afcc29697f3c2bc7
qjd-yyds <qjd1774@163.com>
2021-12-26 21:47:25 +0800

    feat: next.config.js 配置 环境变量

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+const widthImages = require('next-images'); // static 文件的图片自动加cnd前缀
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = widthImages({
+  basePath: '/docs', // link 不需要加/docs a标签跳转需要加/docs
   reactStrictMode: true,
-}
+  assertPrefix: isProd ? 'https://cdn.mydomain.com' : '' // cdn前缀
+});
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import CustomLink from '../components/customLink';
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_SERVER_HOST);
   return (
     <>
       <h1>index</h1>
