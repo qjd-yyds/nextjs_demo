d79163668f5dd9118bdc2e7e8fa2a9c6adf446d6
qjd-yyds <qjd1774@163.com>
2021-12-26 22:19:18 +0800

    feat: 自定义app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ 
+  Component, // 当前路由匹配的组件
+  pageProps // 已请求的数据
+  }) {
   return <Component {...pageProps} />
 }
 
